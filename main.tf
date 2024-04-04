747a4231a563e5e789e09340ca688fbaff08b498
satishkumarkrishnan <satishkumar.krishnan83.com>
2024-04-04 16:49:14 +0530

    provisioning the EKS cluster

--- main.tf
+++ main.tf
@@ -28,46 +28,11 @@ resource "aws_eks_cluster" "tokyo_EKS" {
   ]
 }
 
-# To create IAM role for EKS
 resource "aws_iam_role" "tokyo_IAM_EKS_role" {
-  name = "tokyo_EKS_role"
-
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
-  assume_role_policy = jsonencode(
-          {
-        "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-             Action = "sts:AssumeRole"
-            "Action": [
-                "eks:ListFargateProfiles",
-                "eks:DescribeNodegroup",
-                "eks:ListNodegroups",
-                "eks:ListUpdates",
-                "eks:AccessKubernetesApi",
-                "eks:ListAddons",
-                "eks:DescribeCluster",
-                "eks:DescribeAddonVersions",
-                "eks:ListClusters",
-                "eks:ListIdentityProviderConfigs",
-                "iam:ListRoles"
-            ],
-            "Resource": "*"
-        },
-        {
-            "Effect": "Allow",
-            "Action": "ssm:GetParameter",
-            "Resource": "arn:aws:ssm:*:515149434592:parameter/*"            
-        }
-    ]
-  })
-
-  tags = {
-    tag-key = "Tokyo_EKS_role"
-  }
+  name               = "tokyo-eks-policy"
+  assume_role_policy = data.aws_iam_policy_document.eks_assume_role_policy.json
 }
+
 #Resource for EKS IAM Role Policy 
 resource "aws_iam_role_policy_attachment" "example-AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
