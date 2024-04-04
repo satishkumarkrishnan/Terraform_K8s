2edd6d3b4268f70ef1e412e879b9d46b33b1fccf
satishkumarkrishnan <satishkumar.krishnan83.com>
2024-04-04 08:29:54 +0530

    provisioning the ASG cluster using K8's

--- main.tf
+++ main.tf
@@ -5,15 +5,6 @@ provider "aws" {
   region = var.region
 }
 
-# Filter out local zones, which are not currently supported 
-# with managed node groups
-data "aws_availability_zones" "available" {
-  filter {
-    name   = "opt-in-status"
-    values = ["opt-in-not-required"]
-  }
-}
-
 module "asg" {
   source="git@github.com:satishkumarkrishnan/terraform-aws-asg.git?ref=main"  
 }
